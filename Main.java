bcb113e3406bba699e6f0e862e7d3567bf4deb49
Teeramanoppin <toprop315@gmail.com>
2025-09-21 22:09:24 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+       
+        Vehicle v1 = new Vehicle("SN12345", 25000, 2025, "Corolla", "XLE", "Toyota");
+       System.out.println(v1);
+     
+    }
+}
 
