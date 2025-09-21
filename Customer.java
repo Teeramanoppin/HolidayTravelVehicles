7a9bbf9d6ffe58b3daff55fd88764b656490c76d
Teeramanoppin <toprop315@gmail.com>
2025-09-21 22:04:40 +0700

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,1 +1,18 @@
+public class Customer {
+    String id;
+    String name;
+    String address;
+    String phone;
+
+    public Customer(String id, String name, String address, String phone) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public String toString() {
+        return id + " - " + name + " - " + address + " - " + phone;
+    }
+}
 
