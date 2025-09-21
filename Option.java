d6ab9a4dff4bcf0d42150d43b46111b4a74a3af5
Teeramanoppin <toprop315@gmail.com>
2025-09-21 22:11:29 +0700

    Update Option.java

--- Option.java
+++ Option.java
@@ -1,1 +1,14 @@
+public class Option {
+    String code;
+    String description;
+    double price;
+  public Option(String code, String description, double price) {
+        this.code = code;
+        this.description = description;
+        this.price = price;
+    }
+    public void showOption() {
+        System.out.println(code + " - " + description + " ($" + price + ")");
+    }
+}
 
