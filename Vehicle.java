5a04d6fcff8a1d5f853ff02cc2d893891f2c55eb
Teeramanoppin <toprop315@gmail.com>
2025-09-21 22:23:38 +0700

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -1,1 +1,25 @@
+public class Vehicle {
+    String serial;
+    String name;
+    String model;
+    String manufacturer;
+    int year;
+    double cost;
+
+   
+    public Vehicle(String serial, String name, String model, String manufacturer, int year, double cost) {
+        this.serial = serial;
+        this.name = name;
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.year = year;
+        this.cost = cost;
+    }
+
+  
+    public void showInfo() {
+        System.out.println(year + " " + manufacturer + " " + name + " " + model +
+                           " [Serial: " + serial + ", Cost: $" + cost + "]");
+    }
+}
 
