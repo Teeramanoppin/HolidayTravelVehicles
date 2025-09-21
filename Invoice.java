09bb12a378949c757ed870f88f24d38419031c51
Teeramanoppin <toprop315@gmail.com>
2025-09-21 22:06:31 +0700

    Update Invoice.java

--- Invoice.java
+++ Invoice.java
@@ -1,1 +1,32 @@
+import java.util.List;
+
+public class Invoice {
+    String id;
+    String date;
+    double total;
+
+    Customer customer;
+    Salesperson salesperson;
+    Vehicle vehicle;
+    List<Option> options;
+
+    public Invoice(String id, String date, double total,
+                   Customer customer, Salesperson salesperson,
+                   Vehicle vehicle, List<Option> options) {
+        this.id = id;
+        this.date = date;
+        this.total = total;
+        this.customer = customer;
+        this.salesperson = salesperson;
+        this.vehicle = vehicle;
+        this.options = options;
+    }
+
+    public String toString() {
+        return id + " | " + date + " | " + total +
+               " | " + customer.name +
+               " | " + salesperson.name +
+               " | " + vehicle.model;
+    }
+}
 
