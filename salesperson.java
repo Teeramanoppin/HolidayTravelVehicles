d2e88c62bfe9de00da12b4687a22a17c69f85516
Teeramanoppin <toprop315@gmail.com>
2025-09-21 22:22:02 +0700

    Update salesperson.java

--- salesperson.java
+++ salesperson.java
@@ -1,1 +1,19 @@
+public class Salesperson {
+    String id;
+    String name;
+    String hireDate;
+
+
+    public Salesperson(String id, String name, String hireDate) {
+        this.id = id;
+        this.name = name;
+        this.hireDate = hireDate;
+    }
+
+ 
+    public void showInfo() {
+        System.out.println("Salesperson: " + name + " (ID: " + id + ", Hire Date: " + hireDate + ")");
+    }
+}
+
 
